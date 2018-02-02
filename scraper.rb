96c9e3e98f0e29a713bc83769b9d704066c48110
Markus Reiter <me@reitermark.us>
2018-02-02 18:03:48 +0100

    Change from Python to Ruby.

---
+++ scraper.rb
@@ -0,0 +1,33 @@
+require 'scraperwiki'
+require 'mechanize'
+
+agent = Mechanize.new
+
+page = agent.get('http://www.fastenergy.at/heizoelpreis-tendenz.htm')
+
+rows = page.search('.trend3 tr:not(:first-child)')
+
+rows = rows.map { |row| row.search('> td') }
+
+rows.each do |row|
+  columns = row.children.reject { |column| column.content.match?(/\A[[:space:]]+\Z/) }
+             .map { |column| column.content.strip }
+
+  name, price_today, price_yesterday, price_difference = columns
+
+  price_per_liter = ->(string) { Float(string.tr('€', '').strip.tr(',', '.')) / 100.0 }
+
+  name = columns[0]
+  id = name.downcase.sub('ä', 'ae').sub('ö', 'oe').sub('ü', 'ue')
+  price_today = price_per_liter.(columns[1])
+  price_yesterday = price_per_liter.(columns[2])
+  price_difference = price_per_liter.(columns[3])
+
+  ScraperWiki.save_sqlite([:id], {
+    id: id,
+    name: name,
+    price_today: price_today,
+    price_yesterday: price_yesterday,
+    price_difference: price_difference,
+  })
+end
