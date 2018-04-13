3a2597080049a0c4d56a52e9592d9c864b7d1d53
Markus Reiter <me@reitermark.us>
2018-04-13 13:56:20 +0200

    Fix parsing for new website design.

--- scraper.rb
+++ scraper.rb
@@ -5,23 +5,23 @@ agent = Mechanize.new
 
 page = agent.get('https://www.fastenergy.at/heizoelpreis-tendenz.htm')
 
-rows = page.search('.trend3 tr:not(:first-child)')
+rows = page.search('#tabelle tbody tr')
 
-rows = rows.map { |row| row.search('> td') }
+rows = rows.map { |row| row.search('> td')  }
 
-rows.each do |row|
-  columns = row.children.reject { |column| column.content.match?(/\A[[:space:]]+\Z/) }
-             .map { |column| column.content.strip }
+rows.each do |columns|
+  name, prices, price_difference = columns
 
-  name, price_today, price_yesterday, price_difference = columns
+  name = name.text.strip
+  price_today, price_yesterday = prices.search('div > div').map(&:text).map(&:strip)
+  price_difference = price_difference.search('div > div').first.text.strip
 
-  price_per_liter = ->(string) { Float(string.tr('€', '').strip.tr(',', '.')) / 100.0 }
+  price_per_liter = ->(string) { Float(string.tr("\u0096", "-").tr(' ', '').tr('€', '').strip.tr(',', '.')) / 100.0 }
 
-  name = columns[0]
   id = name.downcase.sub('ä', 'ae').sub('ö', 'oe').sub('ü', 'ue')
-  price_today = price_per_liter.(columns[1])
-  price_yesterday = price_per_liter.(columns[2])
-  price_difference = price_per_liter.(columns[3])
+  price_today = price_per_liter.(price_today)
+  price_yesterday = price_per_liter.(price_yesterday)
+  price_difference = price_per_liter.(price_difference)
 
   ScraperWiki.save_sqlite([:id], {
     id: id,
